e07edd7dec3cd25609dedd8029b19fdeb32f5492
Bhuban-bhatta-hue <bhuwanbhattaa333@gmail.com>
2025-08-14 11:22:02 +0545

    Add files via upload

---
+++ index.py
@@ -0,0 +1,7 @@
+#pythin is a intrtpreter language it check code line by line at run time.
+print("Hello world!")
+
+#TASK
+#Wap PRINT YOUR NAME
+input=str(input("enter your full name:"))
+print("my name is ",input)
