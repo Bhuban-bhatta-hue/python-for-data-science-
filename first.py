b4707bd75563b60779f163270511e192a5c5f237
Bhuban-bhatta-hue <bhuwanbhattaa333@gmail.com>
2025-08-15 10:13:52 +0545

    Add files via upload

---
+++ first.py
@@ -0,0 +1,8 @@
+#task for HW- input for every data that we discuess and print it
+intiger=int(input("enter the number value:"))
+floats=float(input("enter the decimal value:"))
+string=str(input("enter the values:"))
+
+print("intiger value is :",intiger)
+print("float values is :",floats)
+print("string values is :",string)
